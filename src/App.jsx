9ffedd4272ff1fa221ffb140df78cbf01886e9f7
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-29 15:35:11 +0530

    Learned 2-way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [username, setusername] = useState("Sarthak");
+  const [users, setusers] = useState([
+    { name: "Zuhaib", Age: 17 },
+    { name: "Zuhaib", Age: 17 },
+    { name: "Zuhaib", Age: 17 },
+  ]);
+  const renderUsers = users.map((user, index) => {
+    return (
+      <li key={index}>
+        <span>{user.name}</span> |<small> {user.Age}</small>
+      </li>
+    );
+  });
+  const [fullname, setfullname] = useState("");
+  const [age, setage] = useState(18);
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const newUser = { fullname, age };
+    console.log(newUser);
+  };
+
   return (
     <div>
-      <h1>Changing userName using useStat Hook</h1>
-      <h2>{username}</h2>
-      <button onClick={() => setusername("Zuhaib")}>Click to Change </button>
+      <h1>Register user</h1>
+      <form onSubmit={formSubmit}>
+        <input
+          onChange={(e) => setfullname(e.target.value)}
+          value={fullname}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          onChange={(e) => setage(e.target.value)}
+          value={age}
+          type="number"
+          placeholder="Age"
+        />
+        <button>Submit</button>
+      </form>
+      <hr />
+      <h2>USer Data</h2>
+      <ol>{renderUsers}</ol>
     </div>
   );
 };

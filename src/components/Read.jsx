21c737a721e93d4b4393314ecbad81486c25eace
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-01 11:15:54 +0530

    Done with the overall poject without styling and now first practice all again last time and then start styling the whole project by using tailwindcss also with that i will lrearn integrating tailwind css

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Read = (props) => {
-const todos = props.todos
-const renderTodos = todos.map((todo)=>{
-  return <li key={todo.id}>
-    {todo.title}
-  </li>
-})
+  const todos = props.todos;
+  const settodos = props.settodos;
+  const deletehandeler = (id) => {
+    const filtertodos = todos.filter((todo) => todo.id != id);
+    settodos(filtertodos);
+  };
+  const claerHandler = (id) => {
+    const clearTodos = todos.filter((todo) => todo.id != id);
+    settodos([]);
+  };
+  const renderTodos = todos.map((todo) => {
+    return (
+      <li key={todo.id}>
+        {todo.title} |{" "}
+        <span onClick={() => deletehandeler(todo.id)}>Delete</span>{" "}
+        <span onClick={() => claerHandler(todo.id)}>Clear All</span>
+      </li>
+    );
+  });
+
   return (
     <>
-    <h1>Pendind Todos </h1>
-    <ol>
-      {renderTodos}
-    </ol>
+      <h1>Pendind Todos </h1>
+      <ol>{renderTodos}</ol>
     </>
-  )
-}
+  );
+};
 
-export default Read
+export default Read;

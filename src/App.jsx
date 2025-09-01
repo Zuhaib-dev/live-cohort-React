2a56ee50c24bd23247ef96d97eb2d625a45ad718
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-01 09:16:05 +0530

    Created  todo now it is practice time

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,30 @@ const App = () => {
   const [todos, settodos] = useState([
     { id: 1, title: "Kaam ker ly", isCompleted: false },
   ]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let newTodo = {
+      id: nanoid(),
+      title,
+      isCompleted: false,
+    };
+    let copytodos = [...todos];
+    copytodos.push(newTodo);
+    settodos(copytodos)
+     
+    settitle("")
+    
+  };
+
+  const renderTodos = todos.map(todo => {
+    return <li key={todo.id}> {todo.title}</li>
+  })
+  
+
+
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          let newTodo = {
-            id: nanoid(),
-            title,
-            isCompleted: false,
-          };
-          console.log(newTodo);
-          
-        }}
-      >
+      <form onSubmit={submitHandler}>
         <h1>To-Do</h1>
         <input
           onChange={(e) => settitle(e.target.value)}
@@ -30,6 +40,9 @@ const App = () => {
         <br /> <br />
         <button>Create Todo</button>
       </form>
+      <ol>
+        {renderTodos}
+      </ol>
     </div>
   );
 };

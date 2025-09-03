47ab3bdb793858b26ab31e844b856f95d82ea463
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-03 13:00:11 +0530

    Learned React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from './Components/Home' 
+import Product from './Components/Product' 
+import Service from './Components/Service' 
+import About from './Components/About' 
+import Nav from "./Components/Nav";
 
 const App = () => {
   return (
-    <div className='h-screen w-screen bg-gray-800 text-white'>App</div>
-  )
-}
+    <div className="w-screen h-screen bg-gray-800 text-white">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;

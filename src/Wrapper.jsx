988864236b62f0e6533dbb8df72eb1e0de54a616
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-03 12:06:53 +0530

    Donw with the project Todo now will move forward to learn routing with a  new Project

--- src/Wrapper.jsx
+++ src/Wrapper.jsx
@@ -1,14 +1,14 @@
-import React, { createContext, useState } from 'react'
-export const todoContext = createContext(null)
+import React, { createContext, useState } from "react";
+export const todoContext = createContext(null);
 const Wrapper = (props) => {
-    const [todos, settodos] = useState([
+  const [todos, settodos] = useState([
     { id: 1, title: "Kuch to ker", isCompleted: false },
   ]);
   return (
     <todoContext.Provider value={[todos, settodos]}>
-        {props.children}
+      {props.children}
     </todoContext.Provider>
-  )
-}
+  );
+};
 
-export default Wrapper
+export default Wrapper;

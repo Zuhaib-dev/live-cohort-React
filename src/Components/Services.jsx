23a9b157406c9b65bc7ddfd98acc32e5279e12bb
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-04 11:13:42 +0530

    learned hwo to open a child component in a same page of parent ussing outlet and also plasing the  child route inside the <Route></Route>

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Services = () => {
+  const navigate = useNavigate();
   return (
-    <div>Services</div>
-  )
-}
+    <div className="w-screen h-screen bg-gray-800">
+      <h1 className="text-5xl mb-10">Services</h1>
+      <button
+        onClick={() => navigate("/services/detail")}
+        className="bg-green-500 rounded-2xl px-4 py-2 active:scale-95"
+      >
+        More servicess
+      </button>
+      <hr className="my-10 w-100" />
+      <Outlet />
+    </div>
+  );
+};
 
-export default Services
+export default Services;
--- src/Routes/Mainroutes.jsx
+++ src/Routes/Mainroutes.jsx
@@ -5,6 +5,7 @@ import Products from '../Components/Products'
 import Services from '../Components/Services'
 import About from '../Components/About'
 import ProductDetail from '../Components/ProductsDetail'
+import ServiceDetail from '../Components/ServiceDetail'
 
 const Mainroutes = () => {
   return (
@@ -12,7 +13,10 @@ const Mainroutes = () => {
       <Route path='/' element={<Home />} />
       <Route path='/products' element={<Products />} />
       <Route path='/products/detail/:name' element={<ProductDetail />} />
-      <Route path='/services' element={<Services />} />
+      <Route path='/services' element={<Services />} >
+      <Route path='/services/detail' element={<ServiceDetail />} />
+      </Route>
+      
       <Route path='/about' element={<About />} />
     </Routes>
   )

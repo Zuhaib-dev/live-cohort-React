a5d88d7e1c034487afdaeee9cc3d4b48dfe38196
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-04 15:54:34 +0530

    Deleted whole folder started from scratch to begin with the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import RecipeContext from "./Context/RecipeContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
-  <RecipeContext>
-    <BrowserRouter>
-      <App />
-      <ToastContainer position="top-center" />
-    </BrowserRouter>
-  </RecipeContext>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)

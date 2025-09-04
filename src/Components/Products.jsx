f012a8f95540b0497cbefbe8ca0f3a618bd9aac8
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-04 11:25:39 +0530

    Wil move forward with  a project

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -13,14 +13,14 @@ const Products = () => {
       <div>
         <h1 className="text-4xl mb-4">Product 1</h1>
         <h2 className="text-3xl mb-10">Product Details...</h2>
-        <button onClick={()=> navigatorHandler("Product 1")} className="bg-green-500 text-white px-4 py-2 rounded-xl  active:scale-95">
+        <button onClick={()=> navigatorHandler("Porshe 911 GT3 RS")} className="bg-green-500 text-white px-4 py-2 rounded-xl  active:scale-95">
           Get Details
         </button>
       </div>
       <div className="m-10">
         <h1 className="text-4xl mb-4">Product 2</h1>
         <h2 className="text-3xl mb-10">Product Details...</h2>
-        <button onClick={()=> navigatorHandler("product 2")} className="bg-green-500 text-white px-4 py-2 rounded-xl  active:scale-95">
+        <button onClick={()=> navigatorHandler("Porshe 911 Gt3 RS")} className="bg-green-500 text-white px-4 py-2 rounded-xl  active:scale-95">
           Get Details
         </button>
       </div>

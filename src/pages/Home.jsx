c89c59d50e09a195b9801f3db49e0fe71fb7e4ca
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-05 11:53:30 +0530

    Learned the axios lttle bit also learned how to outsourse the axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Axios from "../utils/Axios";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const getProducts = async () => {
+    try {
+      const  {data}  = await Axios.get("/products/1");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export default Home
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={getProducts} className="bg-green-500 px-4 py-2 rounded-2xl active:scale-95">
+        Get Products
+      </button>
+    </div>
+  );
+};
+
+export default Home;

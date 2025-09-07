346a8f94d53555390d04553b905c8f19fa50ce8e
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-07 12:25:53 +0530

    Practising the redux folder st and boilerplate to move forward

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,9 @@
-import { useEffect } from "react";
-import axios from "./api/axiosconfig";
+import React from 'react'
 
 const App = () => {
-  const getuser = async () => {
-    try {
-      const { data } = await axios.get("/users");
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(()=>{
-    getuser()
-  },[])
+  return (
+    <div>App</div>
+  )
+}
 
-  return <div>App</div>;
-};
-
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { store } from './store/Store.jsx'
-import { Provider } from 'react-redux'
-
+import { store } from "./store/Store.jsx";
+import { Provider } from "react-redux";
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+      <ToastContainer />
+    </Provider>
   </BrowserRouter>
-  <ToastContainer />
-  </Provider>
 );
